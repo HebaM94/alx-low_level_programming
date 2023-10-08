d18632a7e80b2bb178d46de851d9be9c35d7b94c
Heba Magdy <hebamagde@gmail.com>
2023-10-08 16:43:19 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - a program to write alpahbits in lowercase and uppercase using putchar
+*Return: 0 (Success)
+*/
+int main(void)
+{
+char lowercase = 'a';
+char uppercase = 'A';
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+upercase++;
+}
+putchar('\n');
+return (0);
+}
