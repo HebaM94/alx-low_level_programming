487846fe1e2e59cfad0d1981865843f13ff604df
Heba Magdy <hebamagde@gmail.com>
2023-10-20 14:01:10 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* *infinite_add - function that adds two numbers.
+* @n1: first number
+* @n2: 2nd number
+* @r: the buffer that the function will use to store the result
+* @size_r: the buffer size
+* Return: r
+*/
+
