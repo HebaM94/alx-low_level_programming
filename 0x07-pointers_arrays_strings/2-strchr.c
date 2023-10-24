6f0e03e2816aca217462e191ccf801e44eea1cee
Heba Magdy <hebamagde@gmail.com>
2023-10-23 17:55:48 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,19 +3,16 @@
 * *_strchr - function that locates a character in a string
 * @s: string to be checked
 * @c: character we are looking for
-* Return: &s[i] or 0 (NULL - not found)
+* Return: &s[i] or '\0' (NULL - not found)
 */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-if (s[i] != '\0')
-{
-for (; s[i] != '\0'; i++)
+int i;
+for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
 {
 return (&s[i]);
-}
 }
 }
 return ('\0');
