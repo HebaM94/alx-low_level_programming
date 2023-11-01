85968ac2bf120d0eb4dc810a8c91a2e7923a6c37
Heba Magdy <hebamagde@gmail.com>
2023-10-31 18:19:44 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,13 +9,13 @@
 */
 int **alloc_grid(int width, int height)
 {
-int **ar; 
+int **ar;
 int i, j;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-ar = malloc(sizeof(int *) * height);
+ar = malloc(sizeof(int) * height);
 if (ar == NULL)
 {
 return (NULL);
@@ -23,7 +23,6 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 ar[i] = malloc(sizeof(int) * width);
-}
 if (ar[i] == NULL)
 {
 for (; i >= 0; i--)
@@ -32,6 +31,7 @@ free(ar[i]);
 }
 free(ar);
 return (NULL);
+}
 }
 for (i = 0; i < height; i++)
 {
