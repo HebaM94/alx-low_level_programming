4240bb214a20a3011ac001266a25256198f89dc5
“HebaM94” <hebamagde@gmail.com>
2023-11-21 05:39:59 -0800

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* listint_len - function that returns the number of elements
+* in a linked listint_t list
+* @h: linked list to be checked
+* Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
+}
+
