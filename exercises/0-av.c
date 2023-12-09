61d6c3133d72870d11de33df07b89bb3b89ac7df
Heba <hebamagde@gmail.com>
2023-12-09 02:31:51 +0200

    update

--- exercises/0-av.c
+++ exercises/0-av.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
 * main - prints all arguments it receives.
-* @ac: number of arguments
-* @av: array of arguments
+* @argc: number of arguments
+* @argv: array of arguments
 * Return:  0 (Success)
 */
 int main(int ac __attribute__((unused)), char *av[])
 {
-    char **arg = av; // Pointer to the start of the arguments
-
-    // Iterate through arguments until NULL is encountered
-    while (*arg) {
-        printf("%s\n", *arg);
-        arg++; // Move to the next argument
-    }
+int i;
+for (i = 0; av[i]; i++)
+{
+printf("%s\n", av[i]);
+}
 return (0);
 }
