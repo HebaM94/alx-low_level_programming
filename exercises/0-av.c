bc1ca7a907e648d99c819248ed7177b02fe7b5e1
Heba <hebamagde@gmail.com>
2023-12-09 02:39:01 +0200

    update

--- exercises/0-av.c
+++ exercises/0-av.c
@@ -5,12 +5,12 @@
 * @argv: array of arguments
 * Return:  0 (Success)
 */
-int main(int ac __attribute__((unused)), char **av[])
+int main(int ac __attribute__((unused)), char *av[])
 {
 int i, j = 0;
-while (av)
-j++;
-for (i = 0; i<j; i++)
+/**while (av)
+j++;*/
+for (i = 0; av[i]; i++)
 {
 printf("%s\n", av[i]);
 }
