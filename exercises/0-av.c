e124ec5483d0895f07115012284bafa1520a5643
Heba <hebamagde@gmail.com>
2023-12-09 02:28:33 +0200

    update

--- exercises/0-av.c
+++ exercises/0-av.c
@@ -5,7 +5,7 @@
 * @av: array of arguments
 * Return:  0 (Success)
 */
-int main(int ac __attribute__((unused)), char **av[])
+int main(int ac __attribute__((unused)), char *av[])
 {
     char **arg = av; // Pointer to the start of the arguments
 
