6d92ea5a4d306ea204707f979910eccc7c6c3d19
“HebaM94” <hebamagde@gmail.com>
2024-01-11 21:34:40 -0800

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,19 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
-/** main - the main program
+/**
+* main - the main program
 * Return: 0 (Success)
 */
-int main()
+int main(void)
 {
-char a[10],c[10];
-int i,j,k=0;
+char a[10], c[10];
+int i, j, k = 0;
 printf("#Keygen by b44nz0r\n\n");
 while (k < 5 || k >= 10)
 {
-if (k !=0)
+if (k != 0)
 printf("\nThe username length should be 5 to 10 alphabets\n");
 printf("enter username: ");
-scanf("%s",a);
+scanf("%s", a);
 k = strlen(a);
 }
 i = k - 1;
@@ -25,7 +26,7 @@ i--;
 j++;
 }
 c[j] = 0;
-printf("\nThe password is %s\n",c);
+printf("\nThe password is %s\n", c);
 printf("\nHit Enter to Exit\n");
 getchar();
 getchar();
