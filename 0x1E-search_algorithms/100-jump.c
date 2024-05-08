0c70cea537562485a0082ee28a8edd2871ea63f6
Heba <hebamagde@gmail.com>
2024-05-08 08:10:54 +0300

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,22 +13,22 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-int index, m, k, prev;
-if (array == NULL || size == 0)
+int jump, sqroot, k, prev;
+if (array == NULL)
 return (-1);
-m = (int)sqrt((double)size);
+sqroot = (int)sqrt((double)size);
 k = 0;
-prev = index = 0;
+prev = jump = 0;
 do {
-printf("Value checked array[%d] = [%d]\n", index, array[index]);
-if (array[index] == value)
-return (index);
+printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
+if (array[jump] == value)
+return (jump);
 k++;
-prev = index;
-index = k * m;
-} while (index < (int)size && array[index] < value);
-printf("Value found between indexes [%d] and [%d]\n", prev, index);
-for (; prev <= index && prev < (int)size; prev++)
+prev = jump;
+jump = k * sqroot;
+} while (jump < (int)size && array[jump] < value);
+printf("Value found between indexes [%d] and [%d]\n", prev, jump);
+for (; prev <= jump && prev < (int)size; prev++)
 {
 printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 if (array[prev] == value)
