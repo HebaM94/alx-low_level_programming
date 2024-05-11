d17753be22bcd1db4f713a8fba0f3605f0c4921c
Heba <hebamagde@gmail.com>
2024-05-11 05:45:04 +0300

    update

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -14,21 +14,21 @@ int bin_search(int *array, size_t low, size_t high, int value);
  */
 int exponential_search(int *array, size_t size, int value)
 {
-size_t i;
+size_t low, high, i = 1;
 if (array == NULL)
 return (-1);
 if (array[0] != value)
 {
-i = 0;
 while (i < size && array[i] <= value)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 i *= 2;
 }
 }
-printf("Value found between indexes [%ld] and [%ld]\n", i / 2,
-(i < size ? i : size - 1));
-return (bin_search(array, i  / 2, (i < size ? i : size - 1), value));
+low = i / 2;
+high = (i < size ? i : size - 1);
+printf("Value found between indexes [%ld] and [%ld]\n", low, high);
+return (bin_search(array, low, high, value));
 }
 /**
  * bin_search - Searches for a value in a sorted array
@@ -49,18 +49,20 @@ return (-1);
 while (low <= high)
 {
 printf("Searching in array: ");
-for (i = low; i < high; i++)
+for (i = low; i <= high; i++)
 {
 printf("%d", array[i]);
+if (i < high)
+printf(", ");
 }
 printf("%d\n", array[i]);
 mid = low + (high - low) / 2;
 if (array[mid] == value)
 return (mid);
-if (array[mid] > value)
-high = mid - 1;
-else
+else if (array[mid] < value)
 low = mid + 1;
+else
+high = mid - 1;
 }
 return (-1);
 }
