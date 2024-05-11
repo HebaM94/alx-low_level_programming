48ffab8de64515b4ff2a16e44e286d4683c18965
Heba <hebamagde@gmail.com>
2024-05-12 01:12:53 +0300

    update

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -45,7 +45,7 @@ half--;
 if (value == array[half])
 {
 if (half > 0)
-return (rec_search(array, half + 1, value));
+return (advanced_binary_recursive(array, half + 1, value));
 return ((int)half);
 }
 if (value < array[half])
