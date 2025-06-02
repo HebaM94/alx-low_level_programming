a93526ea6fa77cf35ef782c01a1372787b34b9c3
Heba <hebamagde@gmail.com>
2025-06-02 06:57:48 +0300

    updates

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,9 +9,9 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
+if (d == NULL)      //not pointing to any memory yet
+d = malloc(sizeof(struct dog));     //any pointer must be assigned a memory size first
+d->name = name;     //derefernece and access
 d->age = age;
 d->owner = owner;
 }
